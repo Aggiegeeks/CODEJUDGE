6eaacf3a6ebf515b680fd0ed34a6023c1cfc2bb0
ncchetan <113266709+ncchetan@users.noreply.github.com>
2022-10-31 21:33:32 -0500

    Added instructor views (#1)
    * added instructor views
    
    * reverting gem file changes and added default page routes

--- codejudge/config/routes.rb
+++ codejudge/config/routes.rb
@@ -1,5 +1,7 @@
 
 Rails.application.routes.draw do
+  resources :questions
+  resources :instructors
   resources :student_groups
   resources :problem_tags
   resources :tags
---
+++ codejudge/test/system/instructors_test.rb
@@ -0,0 +1,39 @@
+require "application_system_test_case"
+
+class InstructorsTest < ApplicationSystemTestCase
+  setup do
+    @instructor = instructors(:one)
+  end
+
+  test "visiting the index" do
+    visit instructors_url
+    assert_selector "h1", text: "Instructors"
+  end
+
+  test "should create instructor" do
+    visit instructors_url
+    click_on "New instructor"
+
+    click_on "Create Instructor"
+
+    assert_text "Instructor was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Instructor" do
+    visit instructor_url(@instructor)
+    click_on "Edit this instructor", match: :first
+
+    click_on "Update Instructor"
+
+    assert_text "Instructor was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Instructor" do
+    visit instructor_url(@instructor)
+    click_on "Destroy this instructor", match: :first
+
+    assert_text "Instructor was successfully destroyed"
+  end
+end
