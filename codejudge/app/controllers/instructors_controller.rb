694e7512a7585618e3a87de2a727ce7921c44830
gokul-sai-doppalapudi <112354294+gokul-sai-doppalapudi@users.noreply.github.com>
2022-11-09 13:30:49 -0600

    Revert "Instructor crud (#6)" (#7)
    This reverts commit 25fb6f0d72d67d96483813b4366855510170eb73.

--- codejudge/app/controllers/instructors_controller.rb
+++ codejudge/app/controllers/instructors_controller.rb
@@ -4,7 +4,6 @@ class InstructorsController < ApplicationController
   # GET /instructors or /instructors.json
   def index
     # @instructors = Instructor.all
-    @groups = Group.where(author_id: cookies.signed[:user_id])
   end
 
   # GET /instructors/1 or /instructors/1.json
