25fb6f0d72d67d96483813b4366855510170eb73
aravindworspace <112588555+aravindworspace@users.noreply.github.com>
2022-11-09 12:26:29 -0600

    Instructor crud (#6)
    * pushing role based routing and dropdown in registration
    
    * fixed user type dropdown issue
    
    * pushing review changes
    
    * instructor crud operations
    
    * object bug solved
    
    Co-authored-by: Chetan Nagavathi Chendrayudu <ncchetan@tamu.edu>

--- codejudge/app/controllers/groups_controller.rb
+++ codejudge/app/controllers/groups_controller.rb
@@ -3,7 +3,8 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    #@groups = Group.all
+    @groups = Group.where(author_id: cookies.signed[:user_id])
   end
 
   # GET /groups/1 or /groups/1.json
@@ -13,6 +14,7 @@ class GroupsController < ApplicationController
   # GET /groups/new
   def new
     @group = Group.new
+    @group.author_id ||= cookies.signed[:user_id]
   end
 
   # GET /groups/1/edit
@@ -23,37 +25,23 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
 
-    respond_to do |format|
       if @group.save
-        format.html { redirect_to group_url(@group), notice: "Group was successfully created." }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
+        redirect_to instructors_path
       end
-    end
   end
 
   # PATCH/PUT /groups/1 or /groups/1.json
   def update
-    respond_to do |format|
+    
       if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: "Group was successfully updated." }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
+        redirect_to instructors_path
       end
-    end
   end
 
   # DELETE /groups/1 or /groups/1.json
   def destroy
-    @group.destroy
-
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: "Group was successfully destroyed." }
-      format.json { head :no_content }
+    if @group.destroy
+      redirect_to instructors_path
     end
   end
 
