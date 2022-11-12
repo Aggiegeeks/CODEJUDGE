c551b00eeaefee99344fd9d32c721ac539801e85
chanduakula-tamu <chanduakula@tamu.edu>
2022-11-12 14:53:29 -0600

    LoginType - Student vs Instructor Page

--- codejudge/app/controllers/registrations_controller.rb
+++ codejudge/app/controllers/registrations_controller.rb
@@ -5,16 +5,25 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
-
   def create
     unless flash[:google_sign_in].nil?
       @user = sign_up_with_google
     else
-      @user = User.new(user_params)
+      @user = User.new(user_params)      
+      @assignment = Assignment.new
+
+      if user_params_2 == "Instructor"
+        @assignment.role_id = 2
+      else 
+        @assignment.role_id = 4
+      end
     end
 
     if @user.save
-      redirect_to root_path, notice: 'Successfully created account'
+      @assignment.user_id = User.find_by(username: user_params[:username]).id
+      if @assignment.save
+        redirect_to root_path, notice: 'Successfully created account'
+      end
     else
       render :new
     end
@@ -24,6 +33,10 @@ class RegistrationsController < ApplicationController
   def user_params
     # strong parameters
     params.require(:user).permit(:email, :username, :password, :password_confirmation, :firstname, :lastname)
+  end
+
+  def user_params_2
+    params.require(:logintype)
   end
 
   def sign_up_with_google
