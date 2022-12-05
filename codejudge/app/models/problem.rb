c19685246dac1c8eb8eedbd546f07d4a711227c8
ncchetan <113266709+ncchetan@users.noreply.github.com>
2022-12-05 16:11:39 -0600

    Fixed major bugs in questions dashboard and registration page (#29)

--- codejudge/app/models/problem.rb
+++ codejudge/app/models/problem.rb
@@ -3,6 +3,7 @@ class Problem < ApplicationRecord
   accepts_nested_attributes_for :test_cases, reject_if: :all_blank, allow_destroy: true
   has_many :attempts
   # has_many :attempts, dependent: :destroy
+  has_many :problem_tag, dependent: :delete_all
 
   def visible_test_cases(problem, role)
     role == :student.to_s ? problem.test_cases.where(example: true) : problem.test_cases
